97c0ee3ba90afe6f0845a1e07c7f1c532b58614f
Ash Taylor <a@zi.vc>
2021-03-22 23:25:23 +0000

    pushhhhheeen

--- src/components/card.js
+++ src/components/card.js
@@ -1,9 +1,17 @@
-import * as React from "react";
+import * as React from "react"
+import * as style from "./card.module.scss"
 
-const Card = ({ uri, title }, key) => {
-  return <a href={uri} key={key}>
-    {title}
+const Card = ({ uri, title, ids, time }, key) => {
+
+  const image = {}
+  const imageUrl = ids[0]
+  image.backgroundImage = `url(${imageUrl}.jpg)`
+
+  return <a href={uri} key={key} className={style.card}>
+    <div className={style.backgroundImage} style={image}>
+      <h3>{title}</h3>
+    </div>
   </a>
 }
 
-export default Card;
+export default Card
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,16 @@
 import * as React from "react"
-import { Link } from "gatsby"
-import { StaticImage } from "gatsby-plugin-image"
 
-import locations from '../locations.json';
+import locations from "../locations.json"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Card from "../components/card"
 
-console.log(locations);
+console.log(locations)
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
-    <h1>Latest spots</h1>
+    <SEO title="Latest car map uk spots" />
     <div style={{ display: "flex", flexWrap: "wrap", margin: "auto -20px" }}>
       {locations.map(Card)}
     </div>
